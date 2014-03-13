6e7e9470e17abb6fe129e73c1b5738b3a30ad6ab
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2014-03-13 23:50:35 +0100

    CubicWeb does not support operator <

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -15,7 +15,7 @@ web = 'http://brainomics.cea.fr'
 
 __depends__ =  {
     'cubicweb': '>= 3.16.1',
-    'cubicweb-brainomics': '< 0.5.0',
+    'cubicweb-brainomics': None,  # actually it should be '< 0.5.0'
     'cubicweb-nologin': None,
 }
 __recommends__ = {}
